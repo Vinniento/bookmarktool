7d03b1ce30b89b96b985b6ff35717b8b46ef72b3
Vincent Schauer <schauervincent@gmail.com>
2022-06-10 17:58:31 +0200

    killing mutations

--- src/main/java/pt/ipp/isep/dei/examples/tdd/basic/domain/Bookmarks.java
+++ src/main/java/pt/ipp/isep/dei/examples/tdd/basic/domain/Bookmarks.java
@@ -23,6 +23,7 @@ public class Bookmarks {
     public boolean checkIfBookmarkExists(URL url) {
         return bookmarkList
                 .stream()
+                .distinct()
                 .anyMatch(bookmark -> bookmark.getUrl().equals(url));
     }
     public void addTagToBookmark(URL url, String tag) {
