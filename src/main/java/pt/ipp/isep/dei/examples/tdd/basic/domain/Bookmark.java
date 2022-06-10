c0fb8b0d2b59a5f65d8b6e5b14f1277f4e29cff8
Vincent Schauer <schauervincent@gmail.com>
2022-06-10 17:24:56 +0200

    US02 ensureSingleTaggingOfBookmark in Bookmark

--- src/main/java/pt/ipp/isep/dei/examples/tdd/basic/domain/Bookmark.java
+++ src/main/java/pt/ipp/isep/dei/examples/tdd/basic/domain/Bookmark.java
@@ -1,17 +1,30 @@
 package pt.ipp.isep.dei.examples.tdd.basic.domain;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bookmark {
 
     URL url;
+    List<String> tags;
 
     public Bookmark(URL url) {
         this.url = url;
+        this.tags = new ArrayList<>();
+
     }
 
     public URL getUrl() {
         return url;
+    }
+
+    public void addTag(String tag) {
+        this.tags.add(tag);
+    }
+
+    public List<String> getTags() {
+        return this.tags;
     }
 }
 
--- src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/BookmarkTest.java
+++ src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/BookmarkTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,5 +22,25 @@ public class BookmarkTest {
         URL result = bookmark.getUrl();
         //assert
         assertEquals(url, result);
+    }
+
+    @Test
+    public void ensureSingleTaggingOfBookmark() throws MalformedURLException {
+
+        URL url = new URL("http://www.google.com");
+        Bookmark bookmark = new Bookmark(url);
+
+        //arrange
+        String firstTag = "firstTag";
+        List<String> expectedResult = Arrays.asList(firstTag);
+
+        //act
+        bookmark.addTag(firstTag);
+
+        List<String> result = bookmark.getTags();
+
+        //assert
+        assertEquals(expectedResult, result);
+
     }
 }
