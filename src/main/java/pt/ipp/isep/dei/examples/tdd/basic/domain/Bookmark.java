3d2870a02e4148935f745f2078a071bd71b7f658
Vincent Schauer <schauervincent@gmail.com>
2022-06-10 17:08:49 +0200

    src/main + classes added

---
+++ src/main/java/pt/ipp/isep/dei/examples/tdd/basic/domain/Bookmark.java
@@ -0,0 +1,9 @@
+package pt.ipp.isep.dei.examples.tdd.basic.domain;
+
+import java.net.URL;
+
+public class Bookmark {
+
+
+}
+
---
+++ src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/BookmarkTest.java
@@ -0,0 +1,13 @@
+package pt.ipp.isep.dei.examples.tdd.basic.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BookmarkTest {
+
+
+}
